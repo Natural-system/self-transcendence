bd8934de13e9b95da59684fbb94e54eda74301e9
Natural-system <lb@zasv.com>
2026-08-29 16:45:46 +0800

    Update short_url.rb

--- _plugins/short_url.rb
+++ _plugins/short_url.rb
@@ -1,15 +1,16 @@
+# _plugins/short_url.rb
 require 'digest'
 
+# 1. 注册最高优先级的 :post_init 钩子（在初始化文章数据时就写入 permalink）
+Jekyll::Hooks.register :posts, :post_init do |post|
+  unique_key = post.relative_path
+  short_hash = Digest::MD5.hexdigest(unique_key)[0..7]
+  post.data['permalink'] = "/p/#{short_hash}/"
+end
+
+# 2. 注册 :pre_render 钩子双重保险
 Jekyll::Hooks.register :posts, :pre_render do |post|
-  # 如果文章 Front Matter 中没有手动指定 permalink
-  unless post.data['permalink']
-    # 提取文章的相对路径作为唯一基准（确保每篇文章算出来的 Hash 是固定且唯一的）
-    unique_key = post.relative_path
-    
-    # 使用 MD5 计算哈希值，并截取前 8 位纯英数短字符
-    short_hash = Digest::MD5.hexdigest(unique_key)[0..7]
-    
-    # 强制将该文章的 permalink 设为：/p/8位短字符/
-    post.data['permalink'] = "/p/#{short_hash}/"
-  end
+  unique_key = post.relative_path
+  short_hash = Digest::MD5.hexdigest(unique_key)[0..7]
+  post.data['permalink'] = "/p/#{short_hash}/"
 end
